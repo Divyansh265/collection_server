c55bed9ac67e2e777a1c7f1f88791a3c2e0989c4
Divyansh Sharma <sharmadivyansh265@gmail.com>
2024-10-17 17:32:37 +0530

    ten

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ app.get("/server-script.js", (req, res) => {
         "itemListElement": collection.products ? collection.products.map(product => ({
           "@type": "Product",
           "name": product.title,
-          "CollectionName":"MyCollection",
+          "CollectionNameABC":"MyCollection",
           "image": product.images ? product.images.map(image => image.src) : [],
           "url": \`https://\${shop}/products/\${product.handle}\`,
           "offers": {
